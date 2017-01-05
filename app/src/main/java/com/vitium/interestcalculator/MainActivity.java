6f0c55a42954ea26979c9be90ef3ea03a76d3050
elixircist23 <elixircist@gmail.com>
2017-01-05 14:22:11 -0500

    function

--- app/src/main/java/com/vitium/interestcalculator/MainActivity.java
+++ app/src/main/java/com/vitium/interestcalculator/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
@@ -23,9 +26,29 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Log.i("sdf", "sdf");
+                //grabbing values from edit text
+                double principal = Double.parseDouble(((EditText)findViewById(R.id.editPrincipal)).getText().toString());
+                double rate = Double.parseDouble(((EditText)findViewById(R.id.editRate)).getText().toString());
+                double time = Double.parseDouble(((EditText)findViewById(R.id.editTime)).getText().toString());
+
+                //grabbing spinner value to see if years or months
+                Spinner mySpinner = (Spinner) findViewById(R.id.spinner2);
+                String text = mySpinner.getSelectedItem().toString();
+                //if months, do the conversion
+                if(text.equals("Months")){
+                    time = time / 12;
+                }
+
+                //calculate the interest, round using format string
+                double A = principal * (1 + ((rate/100) * time));
+                String format = String.format("%1$,.2f", A);
+
+                TextView tv1 = (TextView)findViewById(R.id.simpleInterest);
+                tv1.setText(format);
+
             }
 
         });
+
     }
 }
