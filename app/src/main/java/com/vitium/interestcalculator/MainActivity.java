f4559181f7a063cd07232792d48359a56c7f4d44
elixircist23 <elixircist@gmail.com>
2017-01-07 15:34:30 -0500

    function

--- app/src/main/java/com/vitium/interestcalculator/MainActivity.java
+++ app/src/main/java/com/vitium/interestcalculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,11 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,10 +25,23 @@ public class MainActivity extends AppCompatActivity {
     Spinner mySpinner;
     TextView interest;
 
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+
+        // Set the list's click listener
+
+
         int color = ContextCompat.getColor(this, R.color.icons);
         fab = (FloatingActionButton) findViewById(R.id.myFAB);
         fab.getDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
@@ -32,12 +51,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+
                 //grabbing values from edit text
                 double principal = 0;
                 double rate = 0;
                 double time = 0;
                 try {
-                    principal = Double.parseDouble(((EditText)findViewById(R.id.editPrincipal)).getText().toString());
+                    principal = Double.parseDouble(((EditText)findViewById(R.id.editPrincipalC)).getText().toString());
                     rate = Double.parseDouble(((EditText)findViewById(R.id.editRate)).getText().toString());
                     time = Double.parseDouble(((EditText)findViewById(R.id.editTime)).getText().toString());
                 }
@@ -60,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 interest = (TextView)findViewById(R.id.simpleInterest);
                 interest.setText(format);
 
-                startActivity(new Intent(MainActivity.this,CompoundActivity.class));
+                //startActivity(new Intent(MainActivity.this,CompoundActivity.class));
 
 
             }
